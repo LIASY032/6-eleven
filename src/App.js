511f9d795276d0c4cfc1b4e78d9c7ad3a7119720
LIASY032 <liasy032@mymail.unisa.edu.au>
2021-07-01 19:59:41 +0930

    add a navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import { Shop } from "react-bootstrap-icons";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <>
+        {/* <div className="logo">
+          <Shop></Shop> Six Eleven
+        </div> */}
+        {/* <Nav className="justify-content-end" activeKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/home">Active</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1">Link</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-2">Link</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="disabled" disabled>
+              Disabled
+            </Nav.Link>
+          </Nav.Item>
+        </Nav> */}
+
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#">
+            <Shop></Shop> Six Eleven
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll" className="justify-content-end">
+            <Nav activeKey="/home">
+              <Nav.Item>
+                <Nav.Link href="/home">Active</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-1">Link</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-2">Link</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="disabled" disabled>
+                  Disabled
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="mr-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+      </>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +81,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
