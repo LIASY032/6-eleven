4bfa7b1014c6c31275d713dcf9d209c5025fa3e2
LIASY032 <liasy032@mymail.unisa.edu.au>
2021-07-30 18:05:55 +0930

    (\￣(エ)￣)backend: admin, async, and auth added

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const config = require("config");
-const { itemSchema } = require("./item");
+// const { itemSchema } = require("./item");
 
 const userSchema = new mongoose.Schema({
   name: {
